fe7a47c3be73d76ed8e1a3a4211ee9a3e053e604
Dezorgon <superemail003@gmail.com>
2021-05-17 21:48:57 +0300

    Docker

--- userService/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'the random string bla bla'
-db = SQLAlchemy(app)
